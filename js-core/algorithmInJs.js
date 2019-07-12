86b39d45cc8c8cf19327246b3bcf9079f8b24267
shadabali604 <shadabali604@gmail.com>
2019-07-12 09:56:58 +0530

    Few Updates in Algorithm File

--- js-core/algorithmInJs.js
+++ js-core/algorithmInJs.js
@@ -1,48 +1,40 @@
 function p(a){   
- var sum =0;
- for(i =0; i < a.length; i++){
-     sum += a[i];
- }
- console.log(sum);
-}
-p([5,555,5,25]);
-
-function simpleArraySum(ar) {
-    let sum = 0;
-    for (var i = 0; i < ar.length; i++) {
-        sum += ar[i];
+    var sum =0;
+    for(i =0; i < a.length; i++){
+        sum += a[i];
     }
     console.log(sum);
-}
-simpleArraySum([1, 2, 3, 4, 10, 11]);
- 
-
-
-function vowelsAndConsonants(s) {
-    var ar = [];
-    for (i = 0; i < s.length; i++){
-        ar.push(s[i]);
-    }
-    console.log(ar)
+   }
+   p([5,555,5,25]);
+   
+   function simpleArraySum(ar) {
+       let sum = 0;
+       for (var i = 0; i < ar.length; i++) {
+           sum += ar[i];
+       }
+       console.log(sum);
+   }
+   simpleArraySum([1, 2, 3, 4, 10, 11]);
     
-    for (i = 0; i < ar.length; i++){
-         console.log(ar[i]);
-    }
-}
-vowelsAndConsonants("shadabali");
-
-
-function vowelsAndConsonants(s) {
-    let vowels = ["a", "e", "i", "o", "u"];
-
-    for(let v of s) {
-        if(vowels.includes(v))
-            console.log(v);
-    }
+   function grade(num){
+       if(num > 25 && num <= 30){
+           return "A"
+       }
+       else if(num > 20 && num  <= 25){
+           return "B"
+       }
+       else if(num > 15 && num <= 20){
+           return "C"
+       }
+       else if(num > 10 && num <= 15){
+           return "D"
+       }
+       else if(num > 5 && num <= 10 ){
+           return "E"
+       }
+       else if(num => 0 && num <= 5){
+           return "F"
+       }
+   }
+   console.log(grade());
     
-    for(let v of s) {
-        if(!vowels.includes(v))
-            console.log(v);
-    }
-}
-vowelsAndConsonants("monish");
